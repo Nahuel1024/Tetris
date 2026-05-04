1c8b8e3a6c0c87a468530f0260c52a2b314ea21b
Nahuel1024 <narosas@alumno.unlam.edu.ar>
2026-05-04 16:54:16 -0300

    Agregué definición de tetrominós en tetrominos.h e implementación en main para dibujar piezas según selección aleatoria de los 7 tetrominos

--- main.c
+++ main.c
@@ -15,10 +15,8 @@ Entrega:
 #include "GBT/gbt.h"
 #include "pantalla.h"
 #include "paletacolor.h"
-
+#include "tetromino.h"
 #define RESO 0
-#define FILAS 22
-#define COLUMNAS 10
 
 int main()
 {
@@ -27,23 +25,23 @@ int main()
     gbt_aplicar_paleta(paleta, CANT_COLORES, GBT_FORMATO_888); //Aplica la paleta de colores que definimos en paletacolor.h
 
     iniciar_pantalla(RESO);  //Iniciar la pantalla en la RESOLUCION QUE ENVIAMOS POR PARAMETRO POR EL MOMENTO
-
-
-    int tetromino1[4][4] =
-    {
-        {0,3,0,0},      // 3 es el color CIAN  el cual representa al tetromino I como dice en paletacolor.h
-        {0,3,0,0},
-        {0,3,0,0},
-        {0,3,0,0}
-    };
+    int tetromino;
     int pos_x = 0;
     int pos_y = 3;
+    tetromino = rand() % 7;
     while(1)
     {
         gbt_procesar_entrada();
-
-        dibujar(tetromino1, pos_x, pos_y); // DIBUJA EL TETROMINO1 EN LA POS_X 0 Y POS_Y 3 (creo que esto es mejor buscar el centro).
+        if(pos_x == FILAS - 1)
+        {
+          pos_x= 0;
+          tetromino = rand() % 7;
+        }
+        else
+        {
+        dibujar(tetrominos[tetromino], pos_x, pos_y); // DIBUJA EL TETROMINO1 EN LA POS_X 0 Y POS_Y 3 (creo que esto es mejor buscar el centro).
         pos_x++; //Aumento el valor de pos_x entonces cuando vuelve al while, dibuja una fila para abajo.
+        }
 
         gbt_esperar(200);
     }
